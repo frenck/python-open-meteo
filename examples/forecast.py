b92f16f5423f6328dfc27a9fd83b911a6c8b59b6
tieu1991 <tieu1991@users.noreply.github.com>
2024-07-11 16:40:20 -0400

    Add support for current weather variables

--- examples/forecast.py
+++ examples/forecast.py
@@ -3,7 +3,7 @@
 import asyncio
 
 from open_meteo import OpenMeteo
-from open_meteo.models import DailyParameters, HourlyParameters
+from open_meteo.models import CurrentParameters, DailyParameters, HourlyParameters
 
 
 async def main() -> None:
@@ -12,7 +12,12 @@ async def main() -> None:
         forecast = await open_meteo.forecast(
             latitude=52.27,
             longitude=6.87417,
-            current_weather=True,
+            current_weather=False,
+            current=[
+                CurrentParameters.TEMPERATURE_2M,
+                CurrentParameters.WEATHER_CODE,
+                CurrentParameters.APPARENT_TEMPERATURE,
+            ],
             daily=[
                 DailyParameters.SUNRISE,
                 DailyParameters.SUNSET,
